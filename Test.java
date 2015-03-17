804d30f9c9f4d50c926ff4d34ad645e6f5ac0a33
t.korf <t.korf@fz-juelich.de>
2015-03-17 09:48:36 +0100

    First Java class

--- Test.java
+++ Test.java
@@ -1,14 +1,14 @@
 public class Test {
 
 
-public static void main(String[] args){
+	public static void main(String[] args){
 
 
-System.out.println("Test");
+		System.out.println("Test");
 
 
 
-}
+	}
 
 
 }
